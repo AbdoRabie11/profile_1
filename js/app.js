b12e835d25607eb29e893e0999b2bfed27569121
abdoecomerce <abdoelprince12345@gmail.com>
2021-12-24 14:16:15 -0800

    hello world

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(window).scroll(function() {
+    if($(this).scrollTop() >= 200) {
+        $("#navbar").addClass("noTransparent")
+    }else {
+        $("#navbar").removeClass("noTransparent")
+    }
+});
+
+$(document).ready(function(){
+    $('.circle').circleProgress({
+        startAngle: -Math.PI / 2,
+        fill: "#3A81C5"
+    }).on('circle-animation-progress', function(event, progress, stepValue){
+        $(this).find('span').html(Math.round(stepValue * 100) + '%')
+    })
+})
